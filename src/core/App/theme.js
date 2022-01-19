2a3d9c2b2a113a7138a225f11199da85de9bb934
Dominik Blak <bldominikak@gmial.com>
2022-01-19 19:02:10 +0100

    create theme.js

---
+++ src/core/App/theme.js
@@ -0,0 +1,9 @@
+const colorNames = {
+  ScienceBlue: "#0366D6",
+  White: "#FFFFFF",
+  DodgerBlue: "#2188FF",
+  SlateGray: "#6E7E91",
+  MineShaftLighter: "rgba(54, 54, 54, 0.72)",
+  Mercury: "#E5E5E5",
+  MineShaft: "#252525",
+};
