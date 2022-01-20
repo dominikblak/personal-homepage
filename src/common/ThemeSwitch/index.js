8994de1aa1d14b5404ab8bc2ba8c887e2a2e72fc
Dominik Blak <bldominikak@gmial.com>
2022-01-20 19:30:58 +0100

    added breakpoints

--- src/common/ThemeSwitch/index.js
+++ src/common/ThemeSwitch/index.js
@@ -1,1 +1,3 @@
 import { useDispatch, useSelector } from "react-redux";
+import { selectIsDarkTheme, toggleTheme } from "../themeSlice";
+import { Box, Button, Icon, IconWrapper, Text, Wrapper } from "./styled";
