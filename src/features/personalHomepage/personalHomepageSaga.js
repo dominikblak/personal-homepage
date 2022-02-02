5e6c1314d4b1c6c629afa233e0290cfd0ce51f13
Dominik Blak <bldominikak@gmial.com>
2022-02-02 11:42:05 +0100

    personalHomepageSaga

---
+++ src/features/personalHomepage/personalHomepageSaga.js
@@ -0,0 +1,19 @@
+import { takeLatest, call, put, delay } from "redux-saga/effects";
+import { fetchRepositories, fetchRepositoriesSuccess, fetchRepositoriesError } from "./personalHomepageSlice";
+import { getRepositories } from "./personalHomepageAPI";
+
+const loadingDelay = 2_000;
+
+function* fetchRepositoriesHandler({ payload: username }) {
+  try {
+    yield delay(loadingDelay); //just to demo loading
+    const repositories = yield call(getRepositories, username);
+    yield put(fetchRepositoriesSuccess(repositories));
+  } catch (error) {
+    yield put(fetchRepositoriesError());
+  }
+}
+
+export function* personalHomepageSaga() {
+  yield takeLatest(fetchRepositories.type, fetchRepositoriesHandler);
+}
