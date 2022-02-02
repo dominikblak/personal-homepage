2cdbdc02d4aeae11e74ac0d1525055f39fc78c4a
Dominik Blak <bldominikak@gmial.com>
2022-02-02 10:46:24 +0100

    gitHubAPI

---
+++ src/features/personalHomepageAPI.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+const githubAPIBaseURL = "https://api.github.com";
+
+export const getRepositories = (username) =>
+  axios.get(`${githubAPIBaseURL}/users/${username}/repos`).then((response) => response.data);
