517bdb28bdd54323e401fee6281f10ec3075a975
Dominik Blak <bldominikak@gmial.com>
2022-02-17 19:18:56 +0100

    import in index PersonalHomepage

---
+++ src/features/personalHomepage/PersonalHomepage/index.js
@@ -0,0 +1,9 @@
+import { MainInformation } from "./MainInformation";
+import { Skills } from "./Skills";
+import { Container, Icon } from "./styled";
+import { skills, nextSkills } from "./skillsData";
+import { PortFolio } from "./Portfolio";
+import { Footer } from "./Footer";
+import { ThemeSwitch } from "../../../common/ThemeSwitch";
+import toolsIcon from "./toolsIcon.png";
+import rocketIcon from "./rocketIcon.png";
