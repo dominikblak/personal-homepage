c9c1f4d3b096191f4e16d8c14f703671e78c1154
Dominik Blak <bldominikak@gmial.com>
2022-02-23 17:38:11 +0100

    added skills data

---
+++ src/features/personalHomepage/PersonalHomepage/skillsData.js
@@ -0,0 +1,28 @@
+export const skills = [
+  "Semantic & accessible HTML",
+  "Error Handling",
+  "Git",
+  "Reponsive Web Design",
+  "React",
+  "GitHub Pull Requstes & Review",
+  // eslint-disable-next-line no-script-url
+  "JavaScript: ES6+",
+  "React Hooks",
+  "NPM",
+  "Working with API (fetch, axios)",
+  "Redux (Toolkit)",
+  "Immutability",
+  "Promises, Async/Await",
+  "Redux-Saga",
+  "Markdown",
+  "Debugging",
+  "React Router",
+  "Scrum",
+  "CSS Flexbox",
+  "CSS Grid",
+  "Trello",
+  "Teamwork",
+  "CSS BEM convetion",
+];
+
+export const nextSkills = ["React Native", "JS classes", "Node.js", "TypeScript", "React Context"];
