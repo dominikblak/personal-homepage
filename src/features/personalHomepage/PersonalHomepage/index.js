ea1f881d84a010ebe1b1ed2af2f03440a1dbf9ea
Dominik Blak <bldominikak@gmial.com>
2022-02-23 17:20:58 +0100

    personalHompage and icons

--- src/features/personalHomepage/PersonalHomepage/index.js
+++ src/features/personalHomepage/PersonalHomepage/index.js
@@ -7,3 +7,30 @@ import { Footer } from "./Footer";
 import { ThemeSwitch } from "../../../common/ThemeSwitch";
 import toolsIcon from "./toolsIcon.png";
 import rocketIcon from "./rocketIcon.png";
+
+export const PersonalHomepage = () => (
+  <Container>
+    <ThemeSwitch />
+    <MainInformation />
+    <main>
+      <Skills
+        title={
+          <>
+            My skillset includes <Icon src={toolsIcon} alt="" />
+          </>
+        }
+        sills={skills}
+      />
+      <Skills
+        title={
+          <>
+            What I want to learn next <Icon src={rocketIcon} alt="" />
+          </>
+        }
+        sills={nextSkills}
+      />
+      <PortFolio />
+    </main>
+    <Footer />
+  </Container>
+);
