fab601c998b6ec47094e6999a479af2dc3923261
Dominik Blak <bldominikak@gmial.com>
2022-03-03 21:08:01 +0100

    sorted repositoreis

--- src/features/personalHomepage/personalHomepageAPI.js
+++ src/features/personalHomepage/personalHomepageAPI.js
@@ -2,4 +2,4 @@ import axios from "axios";
 const githubAPIBaseURL = "https://api.github.com";
 
 export const getRepositories = (username) =>
-  axios.get(`${githubAPIBaseURL}/users/${username}/repos`).then((response) => response.data);
+  axios.get(`${githubAPIBaseURL}/users/${username}/repos?sort=created`).then((response) => response.data);
